f89202806935add2ed7824ce691e1b2a6fbd1f9d
sorawiss <sorawit.bunnee@gmail.com>
2026-04-25 17:08:56 +0700

    Feat : mockup the 5 questions

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -6,13 +6,48 @@ export interface Question {
 
 export const questionPages: Question[] = [
 	{
-		id: "creative-energy",
-		prompt: "When do you feel most creative during your day?",
-		options: ["Morning", "Afternoon", "Evening", "Late night"],
+		id: "1",
+		prompt: "เพิ่งสตาร์ทได้ไม่ทันไร ทรายก็เข้าเต็มรองเท้าจนเริ่มเจ็บ... เอาไงดีนักวิ่ง?",
+		options: [
+			"สลัดทรายออก",
+			"ขอหยุดดูอาการเจ็บก่อน",
+			"เปลี่ยนท่าวิ่งดู!",
+			"วิ่งต่อไม่แคร์ทราย",
+		],
 	},
 	{
-		id: "creative-goal",
-		prompt: "What do you want this project or survey to help you discover?",
+		id: "2",
+		prompt: "วิ่งมาแล้วเกือบครึ่งทาง แต่น้ำในกระติกจะหมด จะไหวไหมเนี่ย?",
+		options: [
+			"อย่างน้อยก็เหลือ",
+			"หันไปถามเพื่อนข้างๆ ว่าไหวไหม",
+			"มองหาแหล่งน้ำด่วน!",
+			"จิบเท่าที่จำเป็นก่อน",
+		],
+	},
+	{
+		id: "3",
+		prompt: "ระวัง!! ข้างหน้าเป็นรอยแยกทรายถล่ม! จะข้ามยังไงดี?",
+		options: [
+			"เช็กความแข็งแรงของพื้นรอบๆ ก่อน",
+			"เอากระบองเพชรมาต่อเป็นสะพาน",
+			"Better habits",
+			"More confidence",
+		],
+	},
+	{
+		id: "4",
+		prompt: "นักวิ่งข้างๆ เริ่มถอดใจ คุณจะบอกว่า…",
+		options: [
+			"A clearer direction",
+			"New ideas",
+			"Better habits",
+			"More confidence",
+		],
+	},
+	{
+		id: "5",
+		prompt: "ใกล้ถึงเส้นชัย! เจอดีล “รับเงินก้อนใหญ่” ไม่ต้องกลับไปวิ่งให้เหนื่อย ดีลนี้เอาไง",
 		options: [
 			"A clearer direction",
 			"New ideas",
