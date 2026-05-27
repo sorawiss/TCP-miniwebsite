8f0d174b53852bea2e3915b0ce67303056b4e373
sorawiss <sorawit.bunnee@gmail.com>
2026-05-27 13:59:28 +0700

    Feat : add opengraph and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,8 +8,35 @@ import { BackgroundMusic } from "@/components/background-music";
 import { OrientationLock } from "@/components/orientation-lock";
 
 export const metadata: Metadata = {
+	metadataBase: new URL("https://70years.tcp.com"),
 	title: "Desert Marathon Quiz",
 	description: "TCP Desert Marathon personality experience",
+	openGraph: {
+		title: "Desert Marathon Quiz",
+		description: "TCP Desert Marathon personality experience",
+		url: "https://70years.tcp.com",
+		siteName: "Desert Marathon Quiz",
+		images: [
+			{
+				url: "/bottom/story-0.webp",
+				width: 1919,
+				height: 787,
+				alt: "Desert Marathon Quiz",
+			},
+		],
+		locale: "th_TH",
+		type: "website",
+	},
+	twitter: {
+		card: "summary_large_image",
+		title: "Desert Marathon Quiz",
+		description: "TCP Desert Marathon personality experience",
+		images: ["/bottom/story-0.webp"],
+	},
+	icons: {
+		icon: "/logo.png",
+		apple: "/logo.png",
+	},
 };
 
 const HeaventRounded = localFont({
